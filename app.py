05baac48fff29c07acaff29764cc9189ef97005a
seanjr2847 <143934944+seanjr2847@users.noreply.github.com>
2024-02-22 23:48:46 +0900

    dd

--- app.py
+++ app.py
@@ -1,40 +1,44 @@
 from flask import Flask, redirect, url_for, session, render_template
 from authlib.integrations.flask_client import OAuth
 import os
+from flask_session import Session
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.secret_key = os.getenv('SECRET_KEY', 'your_fallback_secret_key')
+
+# Flask-Session 설정
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 oauth = OAuth(app)
 google = oauth.register(
     name='google',
-    client_id='107589597057-nmigg8fd0611grc6cn5lkquegbtgbprl.apps.googleusercontent.com', # Google Cloud에서 받은 클라이언트 ID
-    client_secret='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', # 클라이언트 비밀
+    client_id=os.getenv('GOOGLE_CLIENT_ID'),
+    client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
     access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
     authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
     api_base_url='https://www.googleapis.com/oauth2/v1/',
     userinfo_endpoint='https://www.googleapis.com/oauth2/v3/userinfo',
     client_kwargs={'scope': 'openid profile email'},
 )
-app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('./index.html')
+    return render_template('index.html')
 
-# 로그인 라우트
 @app.route('/login')
 def login():
     redirect_uri = url_for('authorize', _external=True)
     return google.authorize_redirect(redirect_uri)
 
-# Google 로그인 후 콜백 라우트
 @app.route('/authorize')
 def authorize():
     token = google.authorize_access_token()
     resp = google.get('userinfo')
     user_info = resp.json()
-    # 사용자 정보를 화면에 표시하기 위해 간단히 처리합니다.
-    return f'<h1>Welcome, {user_info["name"]}!</h1>'
+    # 사용자 정보를 세션에 저장
+    session['user_info'] = user_info
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
